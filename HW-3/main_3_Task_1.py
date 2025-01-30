da92e2771069ec3ca01e7cd20a69dd0699c4d353
Ivan K <kosi17100@gmail.com>
2025-01-30 19:45:44 +0300

    3.1 compleete

--- HW-3/main_3_Task_1.py
+++ HW-3/main_3_Task_1.py
@@ -1,26 +1,12 @@
-# ### **Задача №1.1. `Wizard` (Волшебник)**
-#
-# Создайте класс **`Wizard`** для моделирования волшебника в Хогвартсе. В классе **`Wizard`** задаются приватные поля:
-# имя волшебника,
-# факультет,
-# уровень магической силы,
-# список известных заклинаний (список объектов класса **`Spell`**),
-# текущий статус (в Хогвартсе или выпущен).
-#
-# Все поля должны быть приватными. Реализуйте следующие методы:
-#
-
-# 4. **Другие методы**:
-#     - **`add_spell(spell: Spell)`**: добавляет заклинание в список известных.
-#     - **`remove_spell(spell: Spell)`**: удаляет заклинание из списка известных.
-#     - **`increase_magic_level(amount: int)`**: увеличивает уровень магической силы на заданное значение (неотрицательное).
-#     - **`__str__()`**: возвращает строку, аккумулирующую состояние всех полей объекта.
-
+from __future__ import annotations
 
 class Wizard:
-    def __init__(self, name: str, house: str, magic_level: int, spells: list, status: str):
+    def __init__(self, name: str, house: str, magic_level: int, status: str, spells=[]):
+
         self.__name = name
         self.__house = house
+        if magic_level < 0:
+            raise ValueError(" магическая сила не может быть отрицательной")
         self.__magic_level = magic_level
         self.__spells = spells
         self.__status = status
@@ -48,23 +34,89 @@ class Wizard:
     def set_status(self, status):
         self.__status = status
 
-    def add_spell(self, spell: Spell):
+    def add_spell(self, spell):
         self.__spells.append(spell)
 
-    def remove_spell(self, spell: Spell):
-        if spell in self.__spells:
-            self.__spells.remove(spell)
-        return "такого заклинания у волшебника нет"
+    def remove_spell(self, spell):
+        if spell not in self.__spells:
+            return "такого заклинания у волшебника нет. удалить невозможно"
+        self.__spells.remove(spell)
 
     def increase_magic_level(self, amount: int):
-        if amount >0:
+        if amount > 0:
             self.__magic_level += amount
         return "введено неверное значение"
 
+    def __str__(self):
+        return (f"Волшебник:  {self.__name} \n "
+                f"Факультет:  {self.__house} \n "
+                f"Магическая сила:{self.__magic_level} \n "
+                f"Владеет заклинаниями: {self.__spells} \n "
+                f"Статус обучения: {self.__status} \n")
+
+### Задача №1.2. **`Spell` (Заклинание)**
+
+# Создайте класс **`Spell`** для моделирования заклинания в Хогвартсе.
+# В классе **`Spell`** задаются приватные поля: название заклинания, уровень сложности (по шкале от 1 до 10), тип заклинания (боевое, лечебное и т.д.), описание.
+# Все поля должны быть приватными.
+# Реализуйте методы: геттеры и сеттеры для каждого приватного поля. Операция вывода на экран (**`__str__`**) должна аккумулировать состояние полей объекта.
+
+class Spell:
+    def __init__(self, title: str, dif_level: int, type: str, description: str):
+        self.__title = title
+        if dif_level<1 and dif_level>10:
+            raise ValueError ("только значения от 1 до 10")
+        self.__dif_level = dif_level
+        self.__type = type
+        self.__description = description
+
+    def get_title(self):
+        return self.__title
+    def get_dif_level(self):
+        return self.__dif_level
+    def get_type(self):
+        return self.__type
+    def get_description(self):
+        return self.__description
+
+    def set_title(self, title):
+        if not isinstance(title, str):
+            raise ValueError("Только строковое значение")
+        self.__title = title
+
+    def set_type(self, type):
+        if not isinstance(type, str):
+            raise ValueError("Только строковое значение")
+        self.__type = type
+
+    def set_dif_level(self, dif_level):
+        if dif_level<1 and dif_level>10:
+            raise ValueError ("только значения от 1 до 10")
+        self.__dif_level = dif_level
+
+    def set_title(self, description):
+        self.__description = description
 
     def __str__(self):
-        return (f"{self.__name} \n "
-                f"{self.__house} \n "
-                f"{self.__magic_level} \n "
-                f"{self.__spells} \n "
-                f"{self.__status} \n")
+        return (f"Название: {self.__title} \n"
+                f"Уровень сложности: {self.__dif_level} \n"
+                f"Тип заклинания: {self.__type} \n"
+                f"Описание заклинания: {self.__description}")
+
+
+spell_water = Spell("Вода", 5, "боевое", "смерч")
+spell_fire = Spell("Огонь", 8, "боевое", "ураган")
+spell_doctor = Spell("Доктор", 2, "лечебное", "здоровье улучшает")
+
+
+w1 = Wizard("Эль", "РП-10", 15, "выпущен")
+print(w1)
+w1.add_spell(spell_doctor.get_title())
+print(w1)
+w1.add_spell(spell_water.get_title())
+print(w1)
+w1.remove_spell(spell_water.get_title())
+print(w1)
+
+w1.remove_spell(spell_doctor.get_title())
+print(w1)
