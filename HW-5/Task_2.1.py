19798982bc9888279d649b2b13ce32dfa6bbf8df
Ivan K <kosi17100@gmail.com>
2025-02-21 18:09:59 +0300

    all..

--- HW-5/Task_2.1.py
+++ HW-5/Task_2.1.py
@@ -13,8 +13,8 @@ class Bank:
         self._elements.remove(element)
 
 class Element(ABC):
-    def __init__(self, type_el):
-        self._type_el = type_el
+    def __init__(self, name):
+        self._name = name
 
     @abstractmethod
     def add(self):
@@ -27,21 +27,27 @@ class Element(ABC):
 
 class Client(Element):
     def __init__(self, name, accounts=None):
-        super().__init__()
-        self._name = name
+        super().__init__(name)
         self._accounts = [] or accounts
 
     def add(self):
         acc = Account()
         self._accounts.append(acc)
 
+    def get_name(self):
+        return self._name
     def remove(self, account):
         self._accounts.remove(account)
 
+    def add_account(self, acc):
+        self._accounts.append(acc)
+
+    def __repr__(self):
+        return f" имя {self.get_name()} счет {self._accounts}"
 
 class Employee(Element):
-    def __init__(self, post):
-        super().__init__()
+    def __init__(self, name, post):
+        super().__init__(name)
         self._post = post or []
 
     def add(self):
@@ -54,14 +60,17 @@ class Employee(Element):
 
 class Cashier(Employee):
     def __init__(self, name):
-        super().__init__()
-        self.__name = name
+        super().__init__(name)
 
     def open_shift(self):
         return True
 
+    def get_name(self):
+        return self._name
+
     def close_shift(self):
         return False
+
 class Manager(Employee):
     def __init__(self, name):
         super().__init__()
@@ -74,15 +83,28 @@ class Manager(Employee):
         pass
 
 
-
 class Account():
-    def __init__(self, number, type_account):
+    def __init__(self, number, type_account, client):
         self._number = number
         self.__type_account = type_account
-        self.__client = Client()
+        self.__client = client
 
 
+    def set_client(self,client):
+        self.__client = client
 
+    def __repr__(self):
+        return f" номер счета {self._number} клиент {self.__client}"
+
+client1 = Client("Bob")
+
+
+deb = Account(254, "debit", client1)
+
+print(deb)
+
+
+print(client1)
 
 
 
