d4d5d2ee1691fa3ba5b7303a18fcdeb261f532f5
Ivan K <kosi17100@gmail.com>
2025-02-26 00:54:11 +0300

    6-1

---
+++ HW-6_GUI/Task_6.1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Home Work 6_1 (date_of_user)")
+root.geometry("400x300")
+
+btn = ttk.Button(text="Click me")
+btn.pack(fill=BOTH, expand=True)
+
+root.mainloop()
